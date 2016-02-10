e6cb2ec4c620e71ca46df554214a21f9102bb7b5
Steven Dao <stevedao@gmail.com>
2016-02-10 00:17:06 -0800

    Use VBOs in GL code

--- MayaUsbReceiver/app/src/main/java/me/sdao/mayausbreceiver/MainActivity.java
+++ MayaUsbReceiver/app/src/main/java/me/sdao/mayausbreceiver/MainActivity.java
@@ -125,6 +125,7 @@ public class MainActivity extends AppCompatActivity {
 
             @Override
             public void onRendererShutdown() {
+                screenQuad.shutdown();
             }
         });
     }
