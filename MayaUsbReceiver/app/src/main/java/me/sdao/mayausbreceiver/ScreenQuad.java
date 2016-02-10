61d69d7f2b4d24184477a310a22818f8f2acf07f
Steven Dao <stevedao@gmail.com>
2016-02-10 00:12:33 -0800

    Use VBOs in GL code

--- MayaUsbReceiver/app/src/main/java/me/sdao/mayausbreceiver/ScreenQuad.java
+++ MayaUsbReceiver/app/src/main/java/me/sdao/mayausbreceiver/ScreenQuad.java
@@ -12,43 +12,24 @@ import java.nio.FloatBuffer;
 
 public class ScreenQuad {
 
-    private static final float mVertices[] = {
-            -1.0f, -1.0f, 0.0f,
-            -1.0f, 1.0f, 0.0f,
-            1.0f, -1.0f, 0.0f,
-            1.0f, 1.0f, 0.0f
-    };
-
-    private static final float mTexCoords[] = {
-            0.0f, 1.0f,
-            0.0f, 0.0f,
-            1.0f, 1.0f,
-            1.0f, 0.0f,
+    private static final float mData[] = {
+            /* x, y, z, s, t */
+            -1.0f, -1.0f, 0.0f, 0.0f, 1.0f,
+            -1.0f, 1.0f, 0.0f, 0.0f, 0.0f,
+            1.0f, -1.0f, 0.0f, 1.0f, 1.0f,
+            1.0f, 1.0f, 0.0f, 1.0f, 0.0f
     };
 
     private boolean mReady;
     private Context mContext;
-    private FloatBuffer mVertexBuffer;
-    private FloatBuffer mTexCoordBuffer;
     private int mProgram;
     private int mProgramPositionParam;
     private int mProgramTexCoordParam;
     private int mProgramBitmapUniform;
-    private int mTextures[] = {-1, -1};
+    private int mTexture;
+    private int mBuffer;
 
     public ScreenQuad(Context context) {
-        ByteBuffer vertexByteBuffer = ByteBuffer.allocateDirect(mVertices.length * 4); // 32 bits.
-        vertexByteBuffer.order(ByteOrder.nativeOrder());
-        mVertexBuffer = vertexByteBuffer.asFloatBuffer();
-        mVertexBuffer.put(mVertices);
-        mVertexBuffer.position(0);
-
-        ByteBuffer texByteBuffer = ByteBuffer.allocateDirect(mTexCoords.length * 4); // 32 bits.
-        texByteBuffer.order(ByteOrder.nativeOrder());
-        mTexCoordBuffer = texByteBuffer.asFloatBuffer();
-        mTexCoordBuffer.put(mTexCoords);
-        mTexCoordBuffer.position(0);
-
         mContext = context;
     }
 
@@ -68,21 +49,57 @@ public class ScreenQuad {
         mProgramTexCoordParam = GLES20.glGetAttribLocation(mProgram, "a_TexCoord");
         mProgramBitmapUniform = GLES20.glGetUniformLocation(mProgram, "u_Bitmap");
 
-        GLES20.glGenTextures(1, mTextures, 0);
+        int[] textures = new int[1];
+        GLES20.glGenTextures(1, textures, 0);
+        mTexture = textures[0];
+
+        int[] buffers = new int[1];
+        GLES20.glGenBuffers(1, buffers, 0);
+        mBuffer = buffers[0];
+        bufferData();
 
         mReady = true;
     }
 
+    public void shutdown() {
+        int[] textures = { mTexture };
+        GLES20.glDeleteTextures(1, textures, 0);
+
+        int[] buffers = { mBuffer };
+        GLES20.glDeleteBuffers(1, buffers, 0);
+
+        mReady = false;
+    }
+
+    private void bufferData() {
+        ByteBuffer dataByteBuffer = ByteBuffer.allocateDirect(mData.length * 4); // 32 bits.
+        dataByteBuffer.order(ByteOrder.nativeOrder());
+        FloatBuffer dataFloatBuffer = dataByteBuffer.asFloatBuffer();
+        dataFloatBuffer.put(mData);
+        dataFloatBuffer.position(0);
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mBuffer);
+
+        GLES20.glBufferData(GLES20.GL_ARRAY_BUFFER,
+                dataFloatBuffer.capacity() * 4 /* bytes per float */,
+                dataFloatBuffer,
+                GLES20.GL_STATIC_DRAW);
+
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+    }
+
     public void bindBitmap(Bitmap bitmap) {
-        if (bitmap == null) {
+        if (bitmap == null || !mReady) {
             return;
         }
 
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextures[0]);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTexture);
 
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MIN_FILTER,
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MIN_FILTER,
                 GLES20.GL_LINEAR);
-        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D, GLES20.GL_TEXTURE_MAG_FILTER,
+        GLES20.glTexParameteri(GLES20.GL_TEXTURE_2D,
+                GLES20.GL_TEXTURE_MAG_FILTER,
                 GLES20.GL_LINEAR);
 
         GLUtils.texImage2D(GLES20.GL_TEXTURE_2D, 0, bitmap, 0);
@@ -96,25 +113,32 @@ public class ScreenQuad {
         GLES20.glUseProgram(mProgram);
 
         GLES20.glActiveTexture(GLES20.GL_TEXTURE0);
-        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTextures[0]);
+        GLES20.glBindTexture(GLES20.GL_TEXTURE_2D, mTexture);
         GLES20.glUniform1i(mProgramBitmapUniform, 0);
 
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, mBuffer);
+
+        GLES20.glEnableVertexAttribArray(mProgramPositionParam);
         GLES20.glVertexAttribPointer(
                 mProgramPositionParam,
                 3 /* coordinates per vertex */,
                 GLES20.GL_FLOAT,
                 false /* normalized */,
-                0 /* stride */,
-                mVertexBuffer);
+                5 * 4 /* stride */,
+                0 /* offset */);
+
+        GLES20.glEnableVertexAttribArray(mProgramTexCoordParam);
         GLES20.glVertexAttribPointer(
                 mProgramTexCoordParam,
                 2 /* coordinates per vertex */,
                 GLES20.GL_FLOAT,
                 false /* normalized */,
-                0 /* stride */,
-                mTexCoordBuffer);
+                5 * 4 /* stride */,
+                3 * 4 /* offset */);
 
-        GLES10.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, mVertices.length / 3);
+        GLES20.glBindBuffer(GLES20.GL_ARRAY_BUFFER, 0);
+
+        GLES10.glDrawArrays(GLES20.GL_TRIANGLE_STRIP, 0, mData.length / 5);
     }
 
 }
